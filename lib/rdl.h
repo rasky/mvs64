80f61ae405ce1c0d470e3794c9dd995d4c0bbee9
Giovanni Bajo <rasky@develer.com>
2021-05-08 10:18:25 +0200

    rdl: increment maximum primitives per rdl_push to 32

--- lib/rdl.h
+++ lib/rdl.h
@@ -187,7 +187,7 @@ typedef uint32_t RdpColor32;
 })
 
 
-#define _get_nth_arg(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, N, ...)  N
+#define _get_nth_arg(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, N, ...)  N
 #define _fe_0(_call, x, ...)
 #define _fe_1(_call, x, y)       _call(x, y)
 #define _fe_2(_call, x, y, ...)  _call(x, y) _fe_1(_call, x, __VA_ARGS__)
@@ -204,8 +204,24 @@ typedef uint32_t RdpColor32;
 #define _fe_13(_call, x, y, ...) _call(x, y) _fe_12(_call, x, __VA_ARGS__)
 #define _fe_14(_call, x, y, ...) _call(x, y) _fe_13(_call, x, __VA_ARGS__)
 #define _fe_15(_call, x, y, ...) _call(x, y) _fe_14(_call, x, __VA_ARGS__)
-#define _count_varargs(...)      _get_nth_arg("ignored", ##__VA_ARGS__, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
-#define _call_multi(...)         _get_nth_arg("ignored", ##__VA_ARGS__, _fe_15, _fe_14, _fe_13, _fe_12, _fe_11, _fe_10, _fe_9, _fe_8, _fe_7, _fe_6, _fe_5, _fe_4, _fe_3, _fe_2, _fe_1, _fe_0)
+#define _fe_16(_call, x, y, ...) _call(x, y) _fe_15(_call, x, __VA_ARGS__)
+#define _fe_17(_call, x, y, ...) _call(x, y) _fe_16(_call, x, __VA_ARGS__)
+#define _fe_18(_call, x, y, ...) _call(x, y) _fe_17(_call, x, __VA_ARGS__)
+#define _fe_19(_call, x, y, ...) _call(x, y) _fe_18(_call, x, __VA_ARGS__)
+#define _fe_20(_call, x, y, ...) _call(x, y) _fe_19(_call, x, __VA_ARGS__)
+#define _fe_21(_call, x, y, ...) _call(x, y) _fe_20(_call, x, __VA_ARGS__)
+#define _fe_22(_call, x, y, ...) _call(x, y) _fe_21(_call, x, __VA_ARGS__)
+#define _fe_23(_call, x, y, ...) _call(x, y) _fe_22(_call, x, __VA_ARGS__)
+#define _fe_24(_call, x, y, ...) _call(x, y) _fe_23(_call, x, __VA_ARGS__)
+#define _fe_25(_call, x, y, ...) _call(x, y) _fe_24(_call, x, __VA_ARGS__)
+#define _fe_26(_call, x, y, ...) _call(x, y) _fe_25(_call, x, __VA_ARGS__)
+#define _fe_27(_call, x, y, ...) _call(x, y) _fe_26(_call, x, __VA_ARGS__)
+#define _fe_28(_call, x, y, ...) _call(x, y) _fe_27(_call, x, __VA_ARGS__)
+#define _fe_29(_call, x, y, ...) _call(x, y) _fe_28(_call, x, __VA_ARGS__)
+#define _fe_30(_call, x, y, ...) _call(x, y) _fe_29(_call, x, __VA_ARGS__)
+#define _fe_31(_call, x, y, ...) _call(x, y) _fe_30(_call, x, __VA_ARGS__)
+#define _count_varargs(...)      _get_nth_arg("ignored", ##__VA_ARGS__, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#define _call_multi(...)         _get_nth_arg("ignored", ##__VA_ARGS__, _fe_31, _fe_30, _fe_29, _fe_28, _fe_27, _fe_26, _fe_25, _fe_24, _fe_23, _fe_22, _fe_21, _fe_20, _fe_19, _fe_18, _fe_17, _fe_16, _fe_15, _fe_14, _fe_13, _fe_12, _fe_11, _fe_10, _fe_9, _fe_8, _fe_7, _fe_6, _fe_5, _fe_4, _fe_3, _fe_2, _fe_1, _fe_0)
 
 #define _rdl_push_1(rdl, prim)   *rdl->__cur++ = prim;
 #define _rdl_push_multi(rdl, ...) ({ \
