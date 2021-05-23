2b7cfd776c41b5382383024b96a86f2eb71de9f9
Giovanni Bajo <rasky@develer.com>
2021-05-24 00:55:56 +0200

    Fix watchdog in N64 version

--- watchdog.c
+++ watchdog.c
@@ -1,7 +1,7 @@
 
 static int watchdog_event;
 
-static unsigned int watchdog_expired(void* arg) {
+static uint32_t watchdog_expired(void* arg) {
 	debugf("[HW] Watchdog expired! Rebooting\n");
 	m68k_pulse_reset();
 	return WATCHDOG_PERIOD;
