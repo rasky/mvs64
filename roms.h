b40966dcd6b968c13c11b770e95e784ffe3007b8
Giovanni Bajo <rasky@develer.com>
2021-05-24 00:58:08 +0200

    Remove obsolete roms loading functions

--- roms.h
+++ roms.h
@@ -6,16 +6,6 @@ extern unsigned int rom_pc_idle_skip;
 
 void rom_load(const char *dir);
 
-void rom_load_bios(const char *dir);
-void rom_load_mslug(const char *dir);
-void rom_load_kof98(const char *dir);
-void rom_load_aof(const char *dir);
-void rom_load_samsho(const char *dir);
-
-void rom_load_spriteex(const char *dir);
-void rom_load_nyanmvs(const char *dir);
-void rom_load_krom(const char *dir);
-
 uint8_t* crom_get_sprite(int spritenum);
 uint8_t* srom_get_sprite(int spritenum);
 
