09438264bd8a23dfaa91b70091e404b2b6f3230a
Giovanni Bajo <rasky@develer.com>
2023-04-11 11:33:21 +0200

    m64k: change default TLB mapping and fix a few bugs uncovered by this

--- m64k/m64k_config.h
+++ m64k/m64k_config.h
@@ -2,7 +2,7 @@
 #define M64K_CONFIG_H
 
 // Base address of the M68K memory space within the N64 memory map
-#define M68K_CONFIG_MEMORY_BASE    0x00000000
+#define M68K_CONFIG_MEMORY_BASE    0xFF000000
 
 // Set to 1 to emulate address exceptions
 #define M64K_CONFIG_ADDRERR        1
